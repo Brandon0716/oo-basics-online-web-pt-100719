c2d0099064d9f993368abc54da1f692b1896be94
essential-robot-4364 <brandon.james414@gmail.com>
2019-11-10 17:52:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,9 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
-  attr_reader :brand
+ # attr_accessor :brand, :color, :size, :material, :condition
+#  attr_reader :brand
   def initialize(brand)
     @brand = brand
   end
   
+  def color
  end
