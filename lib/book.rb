7131cce8cc37d56141a8227bd1cba1f1c8e7d331
essential-robot-4364 <brandon.james414@gmail.com>
2019-11-10 17:39:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,33 +1,35 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
  def initialize(title)
    @title = title
  end
  
- def title
+ # def title
    @title
  end
  
- def author=(author)
+ # def author=(author)
    @author = author
  end
  
- def author
+ # def author
    @author
  end
  
- def page_count=(num)
+# def page_count=(num)
    @page_count = num
  end
  
- def page_count
+ #def page_count
    @page_count
  end
   
-  def genre=(genre)
+  #def genre=(genre)
     @genre = genre
   end
   
-  def genre
+  # def genre
     @genre
   end
   
